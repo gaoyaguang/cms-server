a5cb27e1db91588415fc82e5b85c1253bd792e86
gaoyaguang <1264105348@qq.com>
2017-07-14 10:13:41 +0800

    1231

---
+++ cms-service/src/main/java/com/example/demo/CmsServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsServiceApplication.class, args);
+	}
+}
